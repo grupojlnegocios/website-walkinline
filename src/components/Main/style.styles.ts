b40e38c1457411ea88ba5745f8eb26f2aaff4fd3
Weslley <weslley.washington01486@gmail.com>
2023-05-08 21:09:46 -0300

    Centralizando os Textos

--- src/components/Main/style.styles.ts
+++ src/components/Main/style.styles.ts
@@ -1,16 +1,24 @@
 import styled from 'styled-components'
 
 export const Titulo = styled.div`
-  font-size: 30px;
+  font-size: 40px;
   text-align: center;
 `
 export const Paragrafo = styled.p`
-  text-align: center;
+  display: inline-block;
+  text-align: justify;
+  padding: 45px;
+  max-width: 395px;
 `
 
-export const SessaoPrincipal = styled.section``
+export const SessaoPrincipal = styled.section`
+  display: flex;
+  justify-content: center;
+  margin-top: 3rem;
+`
 
 export const SessaoPrincipalArea = styled.section`
+  position: relative;
   max-width: 50%;
   text-align: center;
 `
@@ -21,4 +29,5 @@ export const Parte1 = styled.h1`
 
 export const Parte2 = styled.h1`
   font-weight: 400;
+  white-space: pre-wrap;
 `

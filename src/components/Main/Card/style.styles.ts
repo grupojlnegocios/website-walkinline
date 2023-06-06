b08e1d964f988aaad0ace46dc6d6dba88139cf28
Weslley <weslley.washington01486@gmail.com>
2023-06-06 17:51:11 -0300

    Colocando sombra nos cards

--- src/components/Main/Card/style.styles.ts
+++ src/components/Main/Card/style.styles.ts
@@ -9,6 +9,7 @@ export const CardBackground = styled.div`
   border-radius: 8px;
   width: 18rem;
   height: 8rem;
+  box-shadow: 0px 11px 18px 1px rgb(0 0 0 / 55%);
   @media (max-width: 1150px) {
     width: 17rem;
     height: 8rem;
